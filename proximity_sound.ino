2f9541c947ffb55fd0d6ca5844d461c8725d5c47
catalinstoian16 <catalinstoian16@gmail.com>
2017-09-24 11:19:33 +0300

    Final version

--- proximity_sound.ino
+++ proximity_sound.ino
@@ -1,18 +1,15 @@
 void setup() {
   pinMode(13,OUTPUT);
-  Serial.begin(9600);
 }
  
 void loop() {
   delay(100);
   int valoareCitita=analogRead(A0);
  
-Serial.println(valoareCitita,DEC);
- 
   if(valoareCitita<100) {
     noTone(13);
   }
   else {
-    tone(13,1000);
+    tone(13,valoareCitita*8);
   }
 }
