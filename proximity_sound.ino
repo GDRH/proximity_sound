a54a446fa2a40bc6e2be17412d8dbeaad12284fa
catalinstoian16 <catalinstoian16@gmail.com>
2017-09-24 10:21:20 +0300

    First version

---
+++ proximity_sound.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  int valoareCitita=analogRead(A0);
+
+  /*  
+   *   aici trebuie editata valoarea citita
+   *   1. trebuie sa devina invers proportionala cu distanta
+   *   2. trebuie sa corespunda intotdeauna unei frecvente care poate fi auzita
+   */
+  
+  noTone(13);
+  tone(13,valoareCitita);
+}
